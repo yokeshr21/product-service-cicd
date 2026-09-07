d20dc4a2ca2bdbefe00a6af94ef4ce11600b1c66
yokeshr21 <yokeshr21@gmail.com>
2026-09-07 15:39:38 +0530

    Test Fixed

--- src/test/java/com/example/product_service_cicd/ProductServiceCicdApplicationTests.java
+++ src/test/java/com/example/product_service_cicd/ProductServiceCicdApplicationTests.java
@@ -2,14 +2,12 @@ package com.example.product_service_cicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ProductServiceCicdApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(1, 2);
 	}
 
 }
