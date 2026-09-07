89950210879338b54566c926b640d7afeadceabe
yokeshr21 <yokeshr21@gmail.com>
2026-09-07 15:16:05 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/product_service_cicd/ProductServiceCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_service_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/product_service_cicd/ProductServiceCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_service_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
